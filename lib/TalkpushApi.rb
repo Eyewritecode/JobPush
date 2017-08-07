971afe872a6d825b56209f47a661bf34f0b92deb
EyeWriteCode <933kss@gmail.com>
2017-08-07 15:00:02 +0900

    added talkpush api class

---
+++ lib/TalkpushApi.rb
@@ -0,0 +1,24 @@
+class TalkpushApi
+  
+  require 'rest-client'
+
+  attr_accessor :url, :campaign_id, :key, :secret
+
+  def initialize(url, campaign_id)
+    @campaign_id = campaign_id
+    @key = Rails.application.secrets.TALKPUSH_KEY
+    @secret = Rails.application.secrets.TALKPUSH_SECRET
+    @url = url
+  end
+    
+  def add_candidate(candidate)
+    puts Rails.application.secrets.TALKPUSH_KEY
+    response = RestClient.post "#{@url}/#{@campaign_id}/campaign_invitations", {api_key: self.key, api_secret: self.secret, campaign_invitation: candidate}
+    
+    if response.code == 200
+      @status = "Sent"
+    else
+      @status = "#{response.code} Error code"
+    end
+  end
+end
